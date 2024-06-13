8ee2d9e39c69e0d66cfeabfacd95c1e7f8538bf2
Phil25687 <schmidpr1@cardiff.ac.uk>
2024-06-13 18:53:19 +0100

    Add files via upload

--- filter.py
+++ filter.py
@@ -8,7 +8,7 @@ ser = serial.Serial('/dev/ttyACM0', 115200) #define port and protocol
 testread = ser.readline().decode('ascii')               #read one line and infer channels off of number of commas
 #channels = testread.count(',')                         #data prototype = timestamp, channel 1, channel 2, ..., channel n (line termination = newline)
 channels = 6
-blocksize = 100                                         #no of samples to be read and processed in each frame
+blocksize = 1000                                        #no of samples to be read and processed in each frame
 LP_cutoff = numpy.array([20, 20, 20, 20, 20, 20])       #cutoff frequency of low pass filter (one per channel)
 filter_order = 4                                        #IIR filter order
 sos = numpy.empty((channels, 2, 6))
@@ -24,6 +24,7 @@ for i in range(2):
     smpls = 0
     data_in = numpy.empty((blocksize, channels + 1))
     data_filtered = numpy.empty((blocksize, channels + 1))
+    data_prefft = numpy.empty((blocksize, channels + 1))
     while smpls < blocksize:
         inBuff = ser.readline().decode('ascii').partition(' ')[0]
         inBuff = numpy.fromstring(inBuff, dtype=float, sep=',')
@@ -37,6 +38,13 @@ for i in range(2):
 
     prev_data = data_filtered[-10:, :]
 
-plt.plot(data_in[10:, 3])
-plt.plot(data_filtered[:, 3])
+    #add ~.1Hz high pass prior to fft to remove DC
+    sos_fft = scipy.signal.butter(filter_order, .1, btype='high', analog=False, output='sos', fs=sensor_samplerate)
+    for c in range(channels):
+        data_prefft[:, c] = scipy.signal.sosfilt(sos_fft, data_in[10:, c]) * numpy.hamming(blocksize)
+    ffts = numpy.fft.fft(data_prefft, 100, 0)
+
+plt.plot(numpy.abs(ffts[:, 3]))
+#plt.plot(data_in[10:, 3])
+#plt.plot(data_filtered[:, 3])
 plt.show()

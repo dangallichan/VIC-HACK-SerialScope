ffe0c2ee06cda1c53720eb5f375e68aa3f082a94
Daniel Gallichan <dgallichan@yahoo.com>
2024-06-14 22:25:00 +0100

    lots of cleanup - added automatic guestimate of data format

--- waveMage.py
+++ waveMage.py
@@ -1,43 +1,37 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Thu Jun 13 15:11:38 2024
-
-@author: epowell
-"""
 import serial
 import serial.tools.list_ports
 import numpy as np
-import sys
+import sys, os, time
 import random
 import matplotlib 
 matplotlib.use('QtAgg')
-import time
+from matplotlib import pyplot as plt
+plt.rcParams['axes.grid'] = True  # Show gridlines by default
+
 from datetime import datetime
-import os
 
 try:
     import Queue
 except:
     import queue as Queue
 
-import numpy as np
-
 from PyQt5 import QtCore, QtWidgets, QtGui
 from PyQt5.QtCore import Qt
 
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 
-# from matplotlib.figure import Figure
-from matplotlib import pyplot as plt
+
 
 SER_TIMEOUT = 2                   # Timeout for serial Rx
 baudrate    = 115200              # Default baud rate
-MAX_N_DATA_CHANNELS = 12
 WINDOW_TITLE = "WaveMage v0.1"
-MINWIDTH, MINHEIGHT = 800, 800
-MAX_X_DATA_RANGE = 10000
+MINWIDTH, MINHEIGHT = 800, 800    # Minimum window size
+MAX_X_DATA_RANGE = 10000          # Maximum number of data points to display on x-axis
+PLOT_REFRESH_RATE = 30            # Refresh rate for plotting in ms (not sure how quickly we can get this to go!)
 
+# Set working directory to the directory of this script for saving data
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 def getSerialPort():
@@ -51,54 +45,77 @@ def getSerialPort():
 
     return port.device
 
-    # To manually override the port selection do something like this:
-    # return "COM11"
-    # return "/dev/ttyUSB0"
-    # return "/dev/ttyACM0"
+# Attempt to automatically find the serial port
+portname = getSerialPort()
+print(portname)
+
+# To manually override the port selection do something like this:
+# portname = "COM11"          ## Windows
+# portname =  "/dev/ttyUSB0"  ## Linux / OSX
+# portname =  "/dev/ttyACM0"  ## Linux / OSX
 
 
-# # Get 10 lines of data at start to check our data stream
-# ser = serial.Serial(getSerialPort(), baudrate, timeout=SER_TIMEOUT)
-# time.sleep(SER_TIMEOUT*1.2)
-# ser.flushInput()
-# for i in range(10):
-#     print(ser.readline().decode("utf-8"))
-# ser.close()
+# Get 10 lines of data at start to check our data stream
+# makes some fairly hideous assumptions about the data format, but tries to guess
+# the number of data channels and whether the first channel is time
+ser = serial.Serial(portname, baudrate, timeout=SER_TIMEOUT)
+time.sleep(SER_TIMEOUT*1.2)
+ser.flushInput()
+testLens = np.zeros(10)
+firstVals = np.zeros(10)
+for i in range(10):
+    thisLine = ser.readline().decode("utf-8")
+    newDataRow = np.array([yy.split(",") for yy in thisLine.split()][0],dtype=float)
+    print(thisLine + " ****  Nvalues: ", len(newDataRow))
+    testLens[i] = len(newDataRow)
+    firstVals[i] = newDataRow[0]
+ser.close()
+n_data_channels = int(np.median(testLens))
+diffFirstVals = np.diff(firstVals)
+meanDiffFirstVals = np.mean(diffFirstVals)
+stderrDiffFirstVals = np.std(diffFirstVals)/meanDiffFirstVals
+print("Diff of first values: ", diffFirstVals)
+print("Stderr of diff of first values: ", stderrDiffFirstVals)
+if stderrDiffFirstVals > 1e-3:
+    print("First values are not consistent, assuming first channel is not time")
+    firstChannelIsTime = False
+else:
+    print("First values are consistent, assuming first channel is time")
+    firstChannelIsTime = True
+    n_data_channels -= 1
+print("Number of data channels: ", n_data_channels)
+
 
 class MainWindow(QtWidgets.QMainWindow):
 
     def __init__(self, *args, **kwargs):
         
-        portname = getSerialPort()
-        print(portname)
-        
         super(MainWindow, self).__init__(*args, **kwargs)
-
 
         layout = QtWidgets.QVBoxLayout()
 
         self.setMinimumSize(MINWIDTH, MINHEIGHT)
         
-        self.n_data_channels = 3
-        self.xScaleFactor = 1e-6
-        self.firstChannelIsTime = True
+        self.n_data_channels = n_data_channels
+        self.xScaleFactor = 1e-6  # currently assumes first channel is time in microseconds
+        self.firstChannelIsTime = firstChannelIsTime
 
-
-        # create plot and put into layout
-        self.fig, self.ax1 = plt.subplots(self.n_data_channels,figsize=(8,5))
+         # create plot and put into layout
+        self.fig, self.ax1 = plt.subplots(self.n_data_channels, 1, sharex=True, figsize=(10, 6))
+        self.ax1[0].set_title('Serial data from ' + portname + ' at ' + str(baudrate) + ' baud')
         self.plotWidget = FigureCanvas(self.fig)
         layout.addWidget(self.plotWidget)
 
         self.n_xpts = 50
+        self.n_xptsAtStart = self.n_xpts
+        self.nPtsAcquired = 0   # keeps track of total number of data points acquired - used also for dealing with first n_xpts data points
 
-        # initialize data
+        # initialize data, xdata as integer counter, ydata as zeros
         self.xdata = np.linspace(1, self.n_xpts, self.n_xpts)
-        self.ydata = np.zeros((self.n_xpts, self.n_data_channels + int(self.firstChannelIsTime)),dtype=float)
+        self.allData = np.zeros((self.n_xpts, self.n_data_channels + int(self.firstChannelIsTime)),dtype=float)
 
-
-        # self.color = iter(plt.cm.rainbow(np.linspace(0, 1, self.n_data_channels)))
+        # color iterator for plotting
         self.color = iter(plt.cm.Set1(np.linspace(0, 1, self.n_data_channels)))
-
         
         # slider for x-axis range
         self.slXDataRange = QtWidgets.QSlider(Qt.Horizontal)
@@ -106,42 +123,54 @@ class MainWindow(QtWidgets.QMainWindow):
         self.slXDataRange.setMaximum(MAX_X_DATA_RANGE)
         self.slXDataRange.setValue(MAX_X_DATA_RANGE-50)
         self.slXDataRange.setTickPosition(QtWidgets.QSlider.TicksBelow)
-        self.slXDataRange.setTickInterval(1)
+        self.slXDataRange.setTickInterval(5)
+        self.slXDataRange.setSingleStep(5)
+        self.slXDataRange.setPageStep(5)
 
         # update along with slider movement
         layout.addWidget(self.slXDataRange)
         self.slXDataRange.valueChanged.connect(self.updateGUI)
 
-        # We need to store a reference to the plotted line
-        # somewhere, so we can apply the new data to it.
-        self._plot_refs = [None] * MAX_N_DATA_CHANNELS
+        # create a reference for each channel plot
+        self._plot_refs = [None] * self.n_data_channels
         for iPlot in range(self.n_data_channels):
-                self._plot_refs[iPlot] = self.ax1[iPlot].plot(self.xdata, self.ydata[-self.n_xpts:,iPlot + int(self.firstChannelIsTime)], color=next(self.color))[0]
+                self._plot_refs[iPlot] = self.ax1[iPlot].plot(self.xdata, self.allData[-self.n_xpts:,iPlot + int(self.firstChannelIsTime)], color=next(self.color))[0]
                 self.ax1[iPlot].set_ylabel('Ch ' + str(iPlot + 1))
 
-        self.serth = SerialThread(portname, baudrate, self.n_data_channels, self.firstChannelIsTime)   # Start serial reading thread
-        self.serth.start()
+        self.serialThread = SerialThread(portname, baudrate, self.n_data_channels, self.firstChannelIsTime)   # Start serial reading thread
+        self.serialThread.start()
 
-        self.serth.signalDataAsMatrix.connect(self.addNewData)
+        self.serialThread.signalDataAsMatrix.connect(self.addNewData)
 
-        self.saveButton = QtWidgets.QPushButton("save those numbers", self)
+        # save data button
+        self.saveButton = QtWidgets.QPushButton("Save data as CSV", self)
+        self.saveButton.setMaximumWidth(200)
         layout.addWidget(self.saveButton)
         self.saveButton.clicked.connect(self.saveData)
 
-        # # have somewhere to display the serial data as text
+        # button to toggle the serial data view
+        self.toggleSerialDataViewButton = QtWidgets.QPushButton("Toggle Live Serial Data View", self)
+        self.toggleSerialDataViewButton.setMaximumWidth(200)
+        layout.addWidget(self.toggleSerialDataViewButton)
+        self.toggleSerialDataViewButton.setCheckable(True)
+
+        # have somewhere to display the serial data as text
+        # For the streaming rates we are using it seems we can get away with just using a text box and continuously appending to it.
         self.serialDataView = SerialDataView(self)
         layout.addWidget(self.serialDataView)
         self.serialDataView.serialData.append("Serial Data:\n")
-        self.serth.signalDataAsString.connect(self.serialDataView.appendSerialText)
+        self.serialThread.signalDataAsString.connect(self.serialDataView.appendSerialText)
+        self.toggleSerialDataViewButton.clicked.connect(self.serialDataView.setVisible)
 
 
         # Setup a timer to trigger the redraw by calling plotData.
         self.timer = QtCore.QTimer()
-        self.timer.setInterval(10)
+        self.timer.setInterval(PLOT_REFRESH_RATE)
         self.timer.timeout.connect(self.plotData)
         self.timer.start()
 
         # central widget for QMainWindow
+        # this is the widget that will contain all our other widgets
         centralWidget = QtWidgets.QWidget()
         centralWidget.setLayout(layout)
         self.setCentralWidget(centralWidget)
@@ -149,27 +178,42 @@ class MainWindow(QtWidgets.QMainWindow):
 
         self.show()
 
-    def addNewData(self, y):
-        # print('received data')
-        self.ydata = np.vstack((self.ydata,y))
+    def addNewData(self, newDataRow):
+        # this function is called when new data is received from the serial port
+        # newDataRow is a numpy array of the new data, emitted by the serial thread
+
+        if self.nPtsAcquired == 0:
+            self.timeAtStart = newDataRow[0]
+
+        if self.nPtsAcquired < self.n_xptsAtStart:
+            self.allData[self.nPtsAcquired,:] = newDataRow
+        else:
+            self.allData = np.vstack((self.allData,newDataRow)) 
+            if not self.firstChannelIsTime:
+                self.xdata = np.linspace(1, self.allData.shape[0], self.allData.shape[0])
+
+        self.nPtsAcquired += 1        
 
     def updateGUI(self):
+        # update the GUI based on the slider position
         self.n_xpts = MAX_X_DATA_RANGE - self.slXDataRange.value()
         self.n_xpts = max(5, self.n_xpts)
 
     def plotData(self):
-    
         # update graphed data for each channel
         for iPlot in range(self.n_data_channels):
-            #print(self.ydata[:,iPlot])
-            # self._plot_refs[iPlot].set_ydata(self.YGlobalScale * self.ydata[:,iPlot] + self.Yoffset*iPlot)
             if self.firstChannelIsTime:
-                self._plot_refs[iPlot].set_xdata(self.ydata[-self.n_xpts:,0] * self.xScaleFactor)
-                self.ax1[iPlot].set_xlim(min(self.ydata[-self.n_xpts:,0] * self.xScaleFactor), max(self.ydata[-self.n_xpts:,0] * self.xScaleFactor))
-            self._plot_refs[iPlot].set_ydata(self.ydata[-self.n_xpts:,iPlot + int(self.firstChannelIsTime)])
-            if np.any(self.ydata[:,iPlot + int(self.firstChannelIsTime)]):
-                self.ax1[iPlot].set_ylim(min(self.ydata[:,iPlot + int(self.firstChannelIsTime)]), max(self.ydata[:,iPlot + int(self.firstChannelIsTime)]))
+                self._plot_refs[iPlot].set_xdata(self.allData[-self.n_xpts:,0] * self.xScaleFactor)
+                self.ax1[iPlot].set_xlim(min(self.allData[-self.n_xpts:,0] * self.xScaleFactor), max(self.allData[-self.n_xpts:,0] * self.xScaleFactor))
             else:
+                self._plot_refs[iPlot].set_xdata(self.xdata[-self.n_xpts:])
+                self.ax1[iPlot].set_xlim(self.nPtsAcquired-self.n_xpts, self.nPtsAcquired)
+            
+            self._plot_refs[iPlot].set_ydata(self.allData[-self.n_xpts:,iPlot + int(self.firstChannelIsTime)])
+
+            if np.any(self.allData[:,iPlot + int(self.firstChannelIsTime)]):
+                self.ax1[iPlot].set_ylim(min(self.allData[:,iPlot + int(self.firstChannelIsTime)]), max(self.allData[:,iPlot + int(self.firstChannelIsTime)]))
+            else:  # handle the case that all data is zero
                 self.ax1[iPlot].set_ylim(-1, 1)
 
         # Trigger the canvas to update and redraw.
@@ -177,10 +221,11 @@ class MainWindow(QtWidgets.QMainWindow):
         self.fig.canvas.flush_events()
 
     def saveData(self):
+       # saves the data to a CSV file
        today = datetime.today()
        timeis = today.strftime("%Y%m%d_%H%M%S")
        fname = str(timeis)+"_data.csv"
-       np.savetxt(fname, self.ydata, fmt='%1.3f')
+       np.savetxt(fname, self.allData, fmt='%1.3f')
        print("saved")
     
 
@@ -190,7 +235,7 @@ class MainWindow(QtWidgets.QMainWindow):
 class SerialThread(QtCore.QThread):
 
     signalDataAsMatrix = QtCore.pyqtSignal(np.ndarray)  # Signal to send data to main thread
-    signalDataAsString = QtCore.pyqtSignal(str)        
+    signalDataAsString = QtCore.pyqtSignal(str)         # Signal to send data to main thread as string
 
     def __init__(self, portname, baudrate, n_data_channels, firstChannelIsTime): # Initialise with serial port details
         QtCore.QThread.__init__(self)
@@ -199,7 +244,7 @@ class SerialThread(QtCore.QThread):
         self.running = True
 
          
-    def run(self):                          # Run serial reader thread
+    def run(self):                          
         print("Opening %s at %u baud " % (self.portname, self.baudrate))
         try:
             self.ser = serial.Serial(self.portname, self.baudrate, timeout=SER_TIMEOUT)
@@ -215,10 +260,10 @@ class SerialThread(QtCore.QThread):
                 thisLine = self.ser.readline().decode("utf-8")
                 # print(thisLine)
                 self.signalDataAsString.emit(thisLine)
-                y = np.array([yy.split(",") for yy in thisLine.split()][0],dtype=float)
-                # print(y)                
-                if len(y) == self.n_data_channels + int(self.firstChannelIsTime):  # Check data is correct length
-                    self.signalDataAsMatrix.emit(y)
+                newDataRow = np.array([yy.split(",") for yy in thisLine.split()][0],dtype=float)
+                # print(newDataRow)                
+                if len(newDataRow) == self.n_data_channels + int(self.firstChannelIsTime):  # Check data is correct length
+                    self.signalDataAsMatrix.emit(newDataRow)
 
             except:
                 pass
@@ -228,6 +273,7 @@ class SerialThread(QtCore.QThread):
             self.ser = None
 
 
+# Class to display serial data as text
 class SerialDataView(QtWidgets.QWidget):
     def __init__(self, parent):
         super(SerialDataView, self).__init__(parent)
@@ -235,14 +281,10 @@ class SerialDataView(QtWidgets.QWidget):
         self.serialData.setReadOnly(True)
         self.serialData.setFontFamily('Courier New')
         self.serialData.setMinimumSize(MINWIDTH, 400)
-        # self.serialData.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
         
     def appendSerialText(self, appendText):
         self.serialData.moveCursor(QtGui.QTextCursor.End)
-        self.serialData.setFontFamily('Courier New')
-        
         self.serialData.insertPlainText(appendText)
-        
         self.serialData.moveCursor(QtGui.QTextCursor.End)
 
 app = QtWidgets.QApplication(sys.argv)
